920a59a1f07ca2cfbb23d0e379c8b7b50719d6f8
Nick <njwymy@gmail.com>
2017-07-01 20:52:00 -0500

    Added week_03 to the mix of things

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #[macro_use]
 extern crate lazy_static;
-
 extern crate tcod;
+extern crate rand;
+
 mod week_01;
 mod week_02;
 mod week_02_ext;
+mod week_03;
 
 fn main() {
-    week_02_ext::run();
+    week_03::run();
 }
