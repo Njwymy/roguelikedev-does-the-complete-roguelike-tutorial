4fdcd4b65dd0741596d3916004bab8d2f4430fcc
Nick <njwymy@gmail.com>
2017-07-16 19:16:57 -0500

    apparently I had all my helper functions in the run function... Moved theses out of there.

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -67,121 +67,126 @@ pub fn run() {
 
     }
 
-    //We need to change which font and some other metadata based on which rendering method we are using
-    //OpenGL is needed on my mac. Otherwize it will render a white screen on startup
-    fn create_root(width:i32, height:i32, ascii_rendering:bool) -> Root {
 
-        if ascii_rendering {
-            let root = Root::initializer()
-                .renderer(Renderer::OpenGL)
-                .size(width,height)
-                .title("Rust/libtcod tutorial")
-                .font("arial10x10.png", FontLayout::Tcod)
-                .font_type(FontType::Greyscale)
-                .init();
-
-                return root;
-
-        }else{
-            
-            let root = Root::initializer()
-                .renderer(Renderer::OpenGL)
-                .size(width,height)
-                .title("Rust/libtcod tutorial")
-                .font("TiledFont.png", FontLayout::Tcod)
-                .font_type(FontType::Greyscale)
-                //#The font has 32 chars in a row, and there's a total of 10 rows. Increase the "10" when you add new rows to the sample font file
-                .font_dimensions(32,10)
-                .init();
-
-            unsafe {
-                //#The index of the first custom tile in the file
-                let mut a = 256;
-                for y in (5..6){
-                    //#The "y" is the row index, here we load the sixth row in the font file. Increase the "6" to load any new rows from the file
-                    tcod_sys::TCOD_console_map_ascii_codes_to_font(a,32,0,y);
-                    a += 32;
-                }  
-            }
-
-            return root;
-
-        }
-    }
     
 
 
 
-    fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map, ascii_rendering:bool){
 
-            //TODO:Maybe instead of branching per interation we can have a method per draw style    
-            for object in objects {
-                if ascii_rendering {
-                    con.set_default_foreground(object.tile.foreground);
-                    con.put_char(object.x, object.y, object.ascii.char, BackgroundFlag::None);
-                }else{
-                    con.put_char_ex(object.x,object.y,object.tile.char, object.tile.foreground, object.tile.background);
-                }
-            }
-            con.set_default_foreground(colors::BLACK);
 
-            for x in 0 .. map.width() {
-                for y in 0 .. map.height() {
-                    let wall = map.at(x,y).is_wall();
-                    let (x,y) = (x as i32, y as i32); //convert from index type usize to map type i32
+    
 
-                    if wall {
-                        con.set_char_background(x,y,COLOR_DARK_WALL,BackgroundFlag::Set);
-                    } else {
-                        con.set_char_background(x,y,COLOR_DARK_GROUND, BackgroundFlag::Set);
 
-                    }
-                }
-            }
+}
 
-            blit(
-                //from
-                con, (0, 0), (con.width(), con.height()), 
-                //to
-                root, (0, 0), 1.0, 1.0);
-            root.flush();
-            
-            con.clear();
-    }
+//We need to change which font and some other metadata based on which rendering method we are using
+//OpenGL is needed on my mac. Otherwize it will render a white screen on startup
+fn create_root(width:i32, height:i32, ascii_rendering:bool) -> Root {
 
-    fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
+    if ascii_rendering {
+        let root = Root::initializer()
+            .renderer(Renderer::OpenGL)
+            .size(width,height)
+            .title("Rust/libtcod tutorial")
+            .font("arial10x10.png", FontLayout::Tcod)
+            .font_type(FontType::Greyscale)
+            .init();
+
+            return root;
+
+    }else{
         
-        //real time
-        //I'm thinking if you want animations outside of a turnbased game this is what you 
-        //would need. (plus emulating your own simulation ticks. (your own turned based code))
-        /*
-        let key = match root.check_for_keypress(tcod::input::KEY_PRESSED){
-            Some(key) => key,
-            None => return false,
-        };
-        */
+        let root = Root::initializer()
+            .renderer(Renderer::OpenGL)
+            .size(width,height)
+            .title("Rust/libtcod tutorial")
+            .font("TiledFont.png", FontLayout::Tcod)
+            .font_type(FontType::Greyscale)
+            //#The font has 32 chars in a row, and there's a total of 10 rows. Increase the "10" when you add new rows to the sample font file
+            .font_dimensions(32,10)
+            .init();
 
-        //turn based
-        let key = root.wait_for_keypress(true);
-
-        match key {
-            Key{code: Up, ..} => player.move_by(map,0,-1),
-            Key{code: Down, ..} => player.move_by(map,0,1),
-            Key{code: Left, ..} => player.move_by(map,-1,0),
-            Key{code: Right, ..} => player.move_by(map,1,0),
-            
-            Key{code: Enter, alt:true, ..} => {
-                let currently_fullscreen = root.is_fullscreen();
-                root.set_fullscreen(!currently_fullscreen);
-            },
-            Key{code: Escape, ..} => {
-                return true
-            }
-            _ => {}, 
+        unsafe {
+            //#The index of the first custom tile in the file
+            let mut a = 256;
+            for y in (5..6){
+                //#The "y" is the row index, here we load the sixth row in the font file. Increase the "6" to load any new rows from the file
+                tcod_sys::TCOD_console_map_ascii_codes_to_font(a,32,0,y);
+                a += 32;
+            }  
         }
 
-        false
+        return root;
+
+    }
+}
+
+fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
+    
+    //real time
+    //I'm thinking if you want animations outside of a turnbased game this is what you 
+    //would need. (plus emulating your own simulation ticks. (your own turned based code))
+    /*
+    let key = match root.check_for_keypress(tcod::input::KEY_PRESSED){
+        Some(key) => key,
+        None => return false,
+    };
+    */
+
+    //turn based
+    let key = root.wait_for_keypress(true);
+
+    match key {
+        Key{code: Up, ..} => player.move_by(map,0,-1),
+        Key{code: Down, ..} => player.move_by(map,0,1),
+        Key{code: Left, ..} => player.move_by(map,-1,0),
+        Key{code: Right, ..} => player.move_by(map,1,0),
+        
+        Key{code: Enter, alt:true, ..} => {
+            let currently_fullscreen = root.is_fullscreen();
+            root.set_fullscreen(!currently_fullscreen);
+        },
+        Key{code: Escape, ..} => {
+            return true
+        }
+        _ => {}, 
     }
 
+    false
+}
 
+fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map, ascii_rendering:bool){
+
+        //TODO:Maybe instead of branching per interation we can have a method per draw style    
+        for object in objects {
+            if ascii_rendering {
+                con.set_default_foreground(object.tile.foreground);
+                con.put_char(object.x, object.y, object.ascii.char, BackgroundFlag::None);
+            }else{
+                con.put_char_ex(object.x,object.y,object.tile.char, object.tile.foreground, object.tile.background);
+            }
+        }
+        con.set_default_foreground(colors::BLACK);
+
+        for x in 0 .. map.width() {
+            for y in 0 .. map.height() {
+                let wall = map.at(x,y).is_wall();
+                let (x,y) = (x as i32, y as i32); //convert from index type usize to map type i32
+
+                if wall {
+                    con.set_char_background(x,y,COLOR_DARK_WALL,BackgroundFlag::Set);
+                } else {
+                    con.set_char_background(x,y,COLOR_DARK_GROUND, BackgroundFlag::Set);
+                }
+            }
+        }
+
+        blit(
+            //from
+            con, (0, 0), (con.width(), con.height()), 
+            //to
+            root, (0, 0), 1.0, 1.0);
+        root.flush();
+        
+        con.clear();
 }
